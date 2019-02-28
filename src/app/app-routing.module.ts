50fb3c8c383090595f41b2d26faeae1924d0ddd5
cheong.kawin@gmail.com <cheong.kawin@gmail.com>
2019-02-28 13:17:10 +0800

    login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,14 @@ import { CreateAccountComponent } from './create-account/create-account.componen
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { TestComponent } from './test/test.component';
 import { LoginComponent } from './login/login.component';
-
-
+import { ExistingUserComponent } from './existing-user/existing-user.component';
 
 const routes: Routes = [
   {path:"" , component: LandingPageComponent},
   {path:"create-account" , component: CreateAccountComponent},
   {path:"test" , component: TestComponent},
-  {path:"login" , component: LoginComponent}
+  {path:"login" , component: LoginComponent},
+  {path:"existing-user" , component: ExistingUserComponent}
 ];
 
 @NgModule({
