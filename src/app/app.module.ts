9898e1d70aa7bcbfb30d3ab57c9c655bd2b24010
cheong.kawin@gmail.com <cheong.kawin@gmail.com>
2019-03-08 18:45:18 +0800

    Firestore authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,12 +21,48 @@ import { ModalModule } from 'ngx-bootstrap/modal';
 import { TestComponent } from './test/test.component';
 import { LoginComponent } from './login/login.component';
 
+import { UserService } from './structure/user.service';
+
 /*Firestore*/
 import { AngularFireModule } from "@angular/fire";
 import { AngularFirestoreModule } from "@angular/fire/firestore";
 import { environment } from 'src/environments/environment.prod';
-import { UserService } from './structure/user.service';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+
+import {FirebaseUIModule} from 'firebaseui-angular';
+import * as firebase from 'firebase/app';
+import * as firebaseui from 'firebaseui';
 /*End of Firestore*/
+
+const firebaseUiAuthConfig: firebaseui.auth.Config = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    {
+      scopes: [
+        'public_profile',
+        'email',
+        'user_likes',
+        'user_friends'
+      ],
+      customParameters: {
+        'auth_type': 'reauthenticate'
+      },
+      provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    },
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    {
+      requireDisplayName: false,
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
+    },
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+  ],
+  tosUrl: '<your-tos-link>',
+  privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
+  credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM
+};
 
 @NgModule({
   declarations: [
@@ -51,7 +87,9 @@ import { UserService } from './structure/user.service';
     AppRoutingModule,
     ModalModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    FirebaseUIModule.forRoot(firebaseUiAuthConfig)
   ],
   providers: [UserService],
   bootstrap: [AppComponent],
--- src/app/existing-user/existing-user.component.ts
+++ src/app/existing-user/existing-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseuiAngularLibraryService } from 'firebaseui-angular';
+import { FirebaseUISignInSuccessWithAuthResult } from 'firebaseui-angular';
+import { FirebaseUISignInFailure } from 'firebaseui-angular';
 
 @Component({
   selector: 'app-existing-user',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExistingUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private firebaseuiAngularLibraryService: FirebaseuiAngularLibraryService) {
+    firebaseuiAngularLibraryService.firebaseUiInstance.disableAutoSignIn();
+  }
 
   ngOnInit() {
   }
+
+  successCallback(signInSuccessData: FirebaseUISignInSuccessWithAuthResult) {
+    console.log("success");
+  }
+  
+  errorCallback(errorData: FirebaseUISignInFailure) {
+    console.log("failed");
+  }  
 
 }
